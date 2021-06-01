4962b9c6b8461b4980bb966b9d4d51b368214c23
AleksandarPaunov <67508193+AleksandarPaunov@users.noreply.github.com>
2021-06-02 02:09:57 +0300

    Performance and optimization (glimpse added)

--- Almara/Controllers/HomeController.cs
+++ Almara/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace Almara.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
